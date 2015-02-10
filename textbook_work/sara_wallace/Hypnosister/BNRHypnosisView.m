b9c70b01d4316b1185ba4839345a13fb49fae751
sbw7272 <sara.b.wallace@gmail.com>
2015-02-09 21:10:12 -0500

    Hypnosister working pre-challenges

---
+++ textbook_work/sara_wallace/Hypnosister/BNRHypnosisView.m
@@ -0,0 +1,21 @@
+//
+//  BNRHypnosisView.m
+//  Hypnosister
+//
+//  Created by Sara Wallace on 2/9/15.
+//  Copyright (c) 2015 Sara Wallace. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
